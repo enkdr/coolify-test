459e105f29af746a66646aa41b9850584b927e02
Raphael Chambers <raphaelchambers@gmail.com>
2024-08-18 10:00:22 -0500

    trying updated docker-compose.yaml

--- app/pg.sql
+++ app/pg.sql
@@ -1,3 +1,9 @@
+CREATE TABLE IF NOT EXISTS items (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT
+);
+
 INSERT INTO items (name, description) VALUES 
 ('Item 1', 'Description of Item 1'),
 ('Item 2', 'Description of Item 2');
